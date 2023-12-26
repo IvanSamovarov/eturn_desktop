4478ef396af5b26a0b3dbf5fa7f4f5f8c0f1e949
itwasjoke <andreyvasiliev1204@gmail.com>
2023-12-26 23:43:09 +0300

    h

--- src/MainPage.java
+++ src/MainPage.java
@@ -4,7 +4,18 @@ import javax.swing.*;
 
 public class MainPage extends JPanel {
     private static final long serialVersionUID = 2L;
-    private JFrame frame;
+
+
+    private Frame findActiveFrame() {
+        Frame[] frames = JFrame.getFrames();
+        for (int i = 0; i < frames.length; i++) {
+            if (frames[i].isVisible()) {
+                return frames[i];
+            }
+        }
+        return null;
+    }
+
 
     public MainPage() {
         setLayout(new BorderLayout());
@@ -12,22 +23,22 @@ public class MainPage extends JPanel {
         jp.setLayout(new GridLayout(0, 2));
         JButton jb = new JButton("Создать очередь");
         jp.add(jb);
-        frame = new JFrame();
         jb.addActionListener(new ActionListener() {
             @Override
                 public void actionPerformed(ActionEvent e) {
-                    CreateQueue create = new CreateQueue();
-                    JFrame frame2 = new JFrame();
-                    frame2.add(create);
-                    frame2.setVisible(true);
-                    frame2.setSize(640, 480);                // задаем размеры окна
-                    frame2.setResizable(false);              // запрещаем менять размер окна
-                    frame2.setLocationRelativeTo(null);
-                    setVisible(false);
-                    frame.dispose();
-                    frame.removeAll();
-                    frame.revalidate();
-                    frame.repaint();
+//                    Frame frame = findActiveFrame();
+//                    if (frame!=null){
+//                        frame.removeAll();
+                        CreateQueue create = new CreateQueue();
+                        jp.removeAll();
+                        jp.add(create);
+                        jp.revalidate();
+                        jp.repaint();
+//                        frame.revalidate();
+//                        frame.repaint();
+//                    }
+
+
                 }
         });
         add(jp);
