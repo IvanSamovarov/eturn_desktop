42a74df93167b91948c041681b0229d35714f8b5
m2margaret <margaritanenarokova@gmail.com>
2023-12-27 20:16:53 +0300

    Жопа

--- src/CreateQueue.java
+++ src/CreateQueue.java
@@ -3,13 +3,47 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class CreateQueue extends JPanel {
-    private static final long serialVersionUID = 2L;
+
     public CreateQueue() {
+
+        JFrame frame = new JFrame("Create Queue");
+        JPanel jp = new JPanel();
+        jp.setLayout(new BoxLayout(jp, BoxLayout.Y_AXIS));
+        JPanel jpButton = new JPanel();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 800);
+
+        JTextArea textMainTop1 = new JTextArea("Создать");
+        Font MainFont1 = new Font(null, Font.BOLD, 15);
+        textMainTop1.setFont(MainFont1);
+        JTextArea textMainTop2 = new JTextArea("название очереди:");
+        JTextField addName = new JTextField();
+        JTextArea textDescription = new JTextArea("описание очереди:");
+        JTextField addDescription = new JTextField();
+        JButton createButton = new JButton("создать");
+        JButton cancelButton = new JButton("отмена");
+
+        jp.add(textMainTop1);
+        jp.add(textMainTop2);
+        jp.add(addName);
+        jp.add(textDescription);
+        jp.add(addDescription);
+        jp.add(createButton);
+        jp.add(cancelButton);
+        frame.getContentPane().add(BorderLayout.NORTH, jp);
+        frame.getContentPane().add(BorderLayout.CENTER, jpButton);
+        frame.setVisible(true);
+
+
+/*
         setLayout(new BorderLayout());
         JPanel jp = new JPanel();
+
         jp.setLayout(new GridLayout(0, 2));
         JButton jb = new JButton("Создать");
         jp.add(jb);
+
+
         jb.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -27,5 +61,6 @@ public class CreateQueue extends JPanel {
             }
         });
         add(jp);
+         */
     }
 }
